557f5d4bc522b5cbb798dbb91b1cf944571d3de6
Radhey <radheshyamb9@gmail.com>
2022-05-20 09:31:32 +0530

    ADDED : Adding Validation on PayrollDTO

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollapp.exceptions;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,13 +14,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
+    private static final String message = "Exception while processing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO>handleMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid Date Format");
+        ResponseDTO responseDTO = new ResponseDTO(message,"Should have date in dd MM YYYY");
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
         List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request0", errMesg);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
-
     }
 }
